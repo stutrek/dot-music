773e15784c01fa8528c14f9e68c4137d3d524975
Stu Kabakoff <sakabako@gmail.com>
2023-11-12 23:02:07 -0500

    get notes without lyrics, filter out background, make short notes pills

--- src/app/parser.tsx
+++ src/app/parser.tsx
@@ -6,30 +6,55 @@ import { wheelsOnBus } from '@/converter/samples/wheels';
 import styles from './parser.module.css';
 import { Fragment } from 'react';
 import { aveMaria } from '@/converter/samples/aveMaria';
+import { mario } from '@/converter/samples/mario';
+
+function alterToString(alter?: string | null) {
+    if (alter === '-1') {
+        return 'b'; // return '♭'; this prints weird
+    }
+    if (alter === '1') {
+        return '#'; // this prints weird return '♯';
+    }
+    return '';
+}
 
 export function Parser() {
-    const notes = converter(aveMaria);
-
-    console.log(notes);
-
+    const parts = converter(wheelsOnBus);
+    console.log(parts);
     return (
         <div className={styles.notes}>
-            {notes.map((note, i) => (
-                <Fragment key={i}>
-                    <span
-                        data-pitch={note.pitch}
-                        data-octave={note.octave}
-                        data-note={note.pitch! + note.octave!}
-                        data-duration={note.duration}
-                        className={`${styles[note.pitch!]} ${styles.note}`}
-                        style={{
-                            minWidth: 40,
-                        }}
-                    >
-                        {note.lyric}
-                    </span>
-                </Fragment>
-            ))}
+            {parts.map((measures, i) => {
+                <h3>Part {i + 1}</h3>;
+                return (
+                    <div key={i}>
+                        {measures.map((notes, i) => {
+                            return (
+                                <div key={i} className={styles.measure}>
+                                    {notes.map((note, i) => (
+                                        <Fragment key={i}>
+                                            <span
+                                                data-pitch={note.pitch}
+                                                data-octave={note.octave}
+                                                data-note={
+                                                    (note.pitch || '') +
+                                                    alterToString(note.alter) +
+                                                    (note.octave || '')
+                                                }
+                                                data-duration={note.duration}
+                                                className={`${styles[note.pitch.toLowerCase()]} ${
+                                                    styles.note
+                                                } ${styles['type' + note.type] || ''}`}
+                                            >
+                                                {note.lyric}
+                                            </span>
+                                        </Fragment>
+                                    ))}
+                                </div>
+                            );
+                        })}
+                    </div>
+                );
+            })}
         </div>
     );
 }
--- src/converter/index.ts
+++ src/converter/index.ts
@@ -1,29 +1,57 @@
+type Note = {
+    pitch: string;
+    alter: string;
+    octave: string;
+    duration: string;
+    lyric?: string;
+    type: string;
+};
+
 function text(node: Element, selector: string) {
-    return node.querySelector(selector)?.textContent;
+    return node.querySelector(selector)?.textContent || '';
 }
 
 function partParser(part: Element) {
-    const key = part.querySelector('key fifths')?.textContent;
-    const notes = Array.from(part.querySelectorAll('note'))
-        .filter((note) => !!text(note, 'lyric'))
-        .map((note) => {
-            const pitch = text(note, 'pitch step')?.toLowerCase();
-            const octave = text(note, 'pitch octave');
-            const duration = text(note, 'duration');
-            const lyric = text(note, 'lyric text');
-            return {
+    // const key = part.querySelector('key fifths')?.textContent;
+    const measureNodes = Array.from(part.querySelectorAll('measure'));
+    const measures: Note[][] = [];
+    for (const measure of measureNodes) {
+        const notes: Note[] = [];
+        for (let i = 0; i < measure.children.length; i++) {
+            const child = measure.children[i];
+            if (child.nodeName === 'backup') {
+                const value = Number(child.textContent || '0');
+                i += value;
+                continue;
+            }
+            if (child.nodeName !== 'note') {
+                continue;
+            }
+            const pitch = text(child, 'pitch step')?.toUpperCase();
+            const octave = text(child, 'pitch octave');
+            const alter = text(child, 'alter');
+            const duration = text(child, 'duration');
+            const type = text(child, 'type');
+            const lyric = text(child, 'lyric text') || undefined;
+            notes.push({
                 pitch,
+                alter,
                 octave,
                 duration,
+                type,
                 lyric,
-            };
-        });
-    return notes;
+                //@ts-ignore
+                child,
+            });
+        }
+        measures.push(notes);
+    }
+    return measures;
 }
 
 export function converter(xmlString: string) {
     const doc = new DOMParser().parseFromString(xmlString, 'text/xml') as XMLDocument;
     console.log(doc);
-    const notes = partParser(doc.querySelectorAll('part')[0]);
-    return notes;
+    const parts = Array.from(doc.querySelectorAll('part')).map(partParser);
+    return parts;
 }
